319874f7327e4c30086b057207f8cb714b826f41
Pawel Kmuk <kmuk.pawel@gmail.com>
2023-12-04 08:31:26 +0100

    revised Hero.jsx in section home

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ return (
             <Route path="/kontakt" element={<Contact />}/>
          </Routes>
          <Footer />
+         <Footer />
+         <Footer />
+         <Footer />
       </div>
    )
 }
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -7,28 +7,42 @@ import photo3 from '../assets/hero/photo3.jpg'
 
 const Hero = () => {
 
-    const [currentBg, setCurrentBg] = useState(photo1)
-    const images = [ photo1, photo2, photo3 ]
-    let i = 0
-
+    const images = [
+      photo1,
+      photo2,
+      photo3,
+    ];
+    const [currentImage, setCurrentImage] = useState(images[0]);
+  
     useEffect(() => {
-        const interval = setInterval(()=> {
-            const nextBg = images[i]
-            setCurrentBg(nextBg)
-            i++
-            if(i >= images.length) (i = 0)
-        }, 4000)
+        let i = 0;
+        const interval = setInterval(() => {
+            setCurrentImage(images[i]);
+            i = (i + 1) % images.length;
+        }, 4000);
         return () => clearInterval(interval);
-    },[])
+    }, []);
 
     return (
         <section
-            className='w-full h-screen bg-cover bg-center'
-            style={{ backgroundImage: `url(${currentBg})` }}
+            className='w-full h-screen relative overflow-hidden z-0'
         >
-            <div className='w-full h-full bg-black/60 flex justify-center items-center'>
+
+            {images.map((image, index) => (
+                <div
+                    key={index}
+                    
+                >
+                    <img
+                        src={image}
+                        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000 ${currentImage === image ? 'z-10 opacity-100' : 'opacity-0 z-0'}`}
+                    />
+                </div>
+            ))}
+
+            <div className='w-full h-full bg-black/60 flex justify-center items-center relative z-20'>
                 
-                <div className=' max-w-[700px] px-3'>
+                <div className=' max-w-[700px] px-3 z-20'>
                     <h1 className='text-xl sm:text-3xl md:text-4xl pb-2 text-slate-100 font-semibold fontHero tracking-wider'>Najprostszy sposób na znalezienie </h1>
                     <h1 className='text-xl sm:text-3xl md:text-4xl pb-6 text-slate-100 font-semibold fontHero tracking-wider'>wymarzonego domu</h1>
                     <div className='flex gap-4'>
